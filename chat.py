2595f62d93e5d20eb9890ac7f16b67cca62da538
Diego Hernandes <diegowalhernandes@gmail.com>
2023-10-01 11:53:36 -0300

    :up: creating the modal

--- chat.py
+++ chat.py
@@ -2,7 +2,23 @@ import flet as ft
 
 def main(pages):
     text = ft.Text("Hello World!")
-    buttonInit = ft.ElevatedButton("Init Chat")
+
+    userName = ft.TextField()
+    modal = ft.AlertDialog(
+        open=False,
+        modal=True,
+        title=ft.Text("Welcome Matrix"),
+        content=userName,
+        actions=[ft.ElevatedButton("Enter")]
+    )
+
+    def enterChat(e):
+        pages.dialog = modal
+        modal.open = True
+        pages.update()
+    
+    buttonInit = ft.ElevatedButton("Enter Chat", on_click=enterChat)
+
     pages.add(text)
     pages.add(buttonInit)
     
