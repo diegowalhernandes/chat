ab5bbb4f51c0cb38c13e18396e0cca5226e45d37
Diego Hernandes <diegowalhernandes@gmail.com>
2023-10-01 13:33:44 -0300

    :up: add text field features

--- chat.py
+++ chat.py
@@ -3,13 +3,35 @@ import flet as ft
 def main(pages):
     text = ft.Text("Hello World!")
 
-    userName = ft.TextField()
+    chat = ft.Column()
+
+    userName = ft.TextField(label="write your name")
+
+    def sendMessage(e):
+        chat.controls.append(ft.Text(mensageField.value))
+
+        mensageField.value = ""
+        pages.update()
+
+    mensageField = ft.TextField(label="type a message")    
+    sendMessageButton = ft.ElevatedButton("Send", on_click=sendMessage)
+
+    def enterModal(e):
+        pages.add(chat)
+        modal.open = False
+        pages.remove(buttonInit)
+        pages.add(ft.Row(
+            [mensageField, sendMessageButton]
+        ))
+        pages.remove(text)
+        pages.update()
+
     modal = ft.AlertDialog(
         open=False,
         modal=True,
         title=ft.Text("Welcome Matrix"),
         content=userName,
-        actions=[ft.ElevatedButton("Enter")]
+        actions=[ft.ElevatedButton("Enter", on_click=enterModal)],
     )
 
     def enterChat(e):
